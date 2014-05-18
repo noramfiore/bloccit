069692296b1d5bd26bd6207b93735e48537d87dd
Nora Fiore <nora.m.fiore@gmail.com>
2014-05-18 17:30:26 -0400

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
   resources :posts
 
   get 'about' => 'welcome#about'
